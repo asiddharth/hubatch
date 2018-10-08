6474416d6b3c1345cb5467707500c95bd70802dd
devamanyu <devamanyu@gmail.com>
2018-10-08 22:19:36 +0800

    Week 8 prelims

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -2,10 +2,11 @@ from .issues import IssueController
 from .organisation import OrganisationController
 from .team_project import TeamProjectMergeStatusDetector
 from .addressbook_pr import AddressbookPRDetector
-from .feedback import CreateFeedback
 from .week_6 import Week_6
 from .general_scripts import General
 from .week_3 import Week_3
 from .week_5 import Week_5
+from .week_7 import Week_7
+from .week_8 import Week_8
 from .track_ta import TADuties
 
