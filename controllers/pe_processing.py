5616359cabdba2d97aa8568bcc70f562e575106d
Siddharth Aravindan <siddharth.aravindan@gmail.com>
2018-11-20 15:28:22 +0800

    Update Regex

--- controllers/pe_processing.py
+++ controllers/pe_processing.py
@@ -27,10 +27,14 @@ FROMREPOORIGINAL = "nus-cs2103-AY1819S1/pe"
 
 GITHUB_ID_COLUMN_INDEX=2 # mapping details: github id
 TEAM_ASSIGNED_COLUMN_INDEX=5 # mapping details: assigned team
-Production = False
+Production = True
 ###############################################################
 REF_TEMPLATE = '\n\n<hr>\n<sub>[original: {}#{}]</sub>'
 
+TO_REPO_TA = "nus-cs2103-AY1819S1/pe-tutor-processing"
+_HEX = '0123456789ABCDEF'
+LINK = "https://github.com/nus-cs2103-AY1819S1/forum/issues"
+PE_issues_regex = 'https://github.com/nus-cs2103-ay1819s1/pe-results/issues/([0-9]+)'
 DUPLICATE_STRING = "Duplicate of #"
 OUTPUT_PATH = "./output/PE-2/"
 OUTPUT_DIR = "./output/"
@@ -41,11 +45,9 @@ MESSAGE_TEMPLATE = "controllers/data/message_template.json"
 Severity_Levels = {"low":0, "medium":1, "high":2}
 CSV_HEADER = ["issue_number", "multiple_responses", "multiple_severity",\
               "multiple_type", "response_missing", "accepted_no_assignees", \
-              "rejected_no_comments", "severity_downgraded_no_comments", \
-              "duplicate_but_no_parent", "is_parent_duplicate", "parent_severity_lesser"]
-
-TO_REPO_TA = "nus-cs2103-AY1819S1/pe-tutor-processing"
-_HEX = '0123456789ABCDEF'
+              "rejected_no_comments", "severity_downgraded_no_comments", "isDuplicate",\
+              "duplicate_but_no_parent", "is_parent_duplicate", "parent_severity_lesser", \
+              "to_post"]
 
 
 with open(MESSAGE_TEMPLATE, 'r') as f:
@@ -56,6 +58,10 @@ class PeProcessing(BaseController):
         self.ghc = ghc
         self.cfg = cfg
         self.gh = Github(self.cfg.get_api_key())
+
+
+        dummy_repo = Github(self.cfg.get_api_key()).get_repo(DUMMY_TOREPO)
+        self.dummy_issue = dummy_repo.get_issue(number=1572)
 
     def setup_argparse(self, subparsers):
         """
@@ -70,8 +76,10 @@ class PeProcessing(BaseController):
 
     def post_pe_issues(self, subparsers):
         parser = subparsers.add_parser('post-checks', help='posts issue checks')
-        parser.add_argument('-audit_csv', '--audit', metavar='csv', type=str,
-                            help='filename of CSV containing issue check audits')
+        parser.add_argument('-csv', type=str,
+                            help='student csv')
+        parser.add_argument('-audit_csv', type=str,
+                            help='pe check details')
         parser.set_defaults(func=self.post_issue_checks)
 
     def process_pe_issues(self, subparsers):
@@ -129,10 +137,11 @@ class PeProcessing(BaseController):
     def extract_team_info(self, csv_file):
 
         user_list=parsers.csvparser.get_rows_as_list(csv_file)[1:]
-        users_to_check=list(map(lambda x: [x[-2].lower().strip(), x[-3], x[-1]], user_list))
+        users_to_check=list(map(lambda x: [x[-2].lower().strip(), x[-3], x[-4], x[0], x[-1]],
+                              user_list))
 
         team_list=defaultdict(list)
-        for user, team, team_no in users_to_check :
+        for user, team, email, name, team_no in users_to_check :
             team_list[team+"-"+team_no[-1]].append(user)
         return team_list
 
@@ -271,16 +280,112 @@ class PeProcessing(BaseController):
     def post_issue_checks(self, args):
         
         # Read audit csv
-        audit = self.read_audit_details(args.audit)
+        audit_details = self.read_audit_details(args.audit_csv)
 
         # Copy all issues
         all_issues = self.get_issues_from_repository()
 
+        # Get all students and mapping from team to students
+        team_list=self.extract_team_info(args.csv)
+
         # Message
         message = message_template["pe-checks"]
 
-        # for issue in all_issues:
-        # issue_obj = self.repo.get_issue(number=issue.number)
+        for issue in all_issues:
+            issue_index=audit_details.index[audit_details['issue_number']==issue.number][0]
+            assert(audit_details["issue_number"][issue_index]==issue.number)
+
+            #Get team details
+            tutorial_id, team_id = None, None
+            for label in issue.labels:
+                if "team" in label.name.lower():
+                    team_id = label.name.split(".")[-1]
+                if "tutorial" in label.name.lower():
+                    tutorial_id = label.name.split(".")[-1]     
+
+            team = tutorial_id+"-"+team_id
+            students = ", ".join(team_list[team])
+            address = team+" "+students
+
+            message_list = []
+            to_print=False
+
+
+            # Duplicate checks
+            printDuplicateCheck=False
+            local_duplicate_message=""
+
+            if (audit_details["duplicate_but_no_parent"][issue_index]):
+                printDuplicateCheck=True
+                local_duplicate_message+=message["parent_not_specified"]
+            else:
+                local_duplicate_message+=""
+
+
+            if (audit_details["is_parent_duplicate"][issue_index]):
+                printDuplicateCheck=True
+                local_duplicate_message+=message["parent_duplicate"]
+            else:
+                local_duplicate_message+=""
+
+            if (audit_details["parent_severity_lesser"][issue_index]):
+                printDuplicateCheck=True
+                local_duplicate_message+=message["parent_lesser"]
+            else:
+                local_duplicate_message+=""
+
+            if printDuplicateCheck:
+                message_list.append(message["duplicate"].format(local_duplicate_message))
+                to_print=True
+
+
+            if (audit_details["multiple_responses"][issue_index]) or \
+               (audit_details["multiple_severity"][issue_index]) or \
+               (audit_details["multiple_type"][issue_index]):
+                message_list.append(message["multiple_labels"])
+                to_print=True
+
+            
+
+            if (not audit_details["isDuplicate"][issue_index]) and\
+               (audit_details["response_missing"][issue_index]):
+                message_list.append(message["missing_response"])
+                to_print=True
+
+
+            if (not audit_details["isDuplicate"][issue_index]) and\
+               (audit_details["accepted_no_assignees"][issue_index]):
+                message_list.append(message["no_assigness"])
+                to_print=True
+
+            if (not audit_details["isDuplicate"][issue_index]) and\
+               (audit_details["rejected_no_comments"][issue_index]):
+                message_list.append(message["rejected_no_comment"])
+                to_print=True
+
+
+            if (audit_details["severity_downgraded_no_comments"][issue_index]):
+                message_list.append(message["downgraded_no_comment"])
+                to_print=True
+
+
+
+
+            # Only if any check is failing
+
+            if int(audit_details["to_post"][issue_index]>0) and to_print:
+                print(issue.number)
+                final_message = message["base"].format(address,"\n".join(message_list).strip(), LINK)
+
+                if Production:
+                    issue_obj = self.repo.get_issue(number=issue.number)
+                    assert(issue.number == issue_obj.number)
+                    issue_obj.create_comment(final_message)
+                    time.sleep(3)
+                else:
+                    self.dummy_issue.create_comment(final_message)
+                    time.sleep(3)
+
 
 
     def read_audit_details(self, path):
@@ -360,8 +465,8 @@ class PeProcessing(BaseController):
             for label in issue.labels:
                 if label.name.lower() == "duplicate":
                     isDuplicateLabel = True
-                    break
-                elif "response" in label.name.lower():
+                
+                if "response" in label.name.lower():
                     if isResponse:
                         isMultipleResponse=True
                     isResponse = True
@@ -371,14 +476,14 @@ class PeProcessing(BaseController):
                     elif "rejected" in label.name.lower():
                         isRejected=True
 
-                elif "severity" in label.name.lower():
+                if "severity" in label.name.lower():
                     if isSeverity:
                         isMultipleSeverity=True
                     isSeverity = True
 
                     issueSeverityCurrent = label.name.split(".")[-1].lower()
 
-                elif "type" in label.name.lower():
+                if "type" in label.name.lower():
                     if isType:
                         isMultipleType=True
                     isType = True
@@ -437,11 +542,13 @@ class PeProcessing(BaseController):
 
 
             if isDuplicateLabel:
+                audit[issue.number].append(1)
 
                 for comment in comments:
                     comment_str = comment.body.lower()
                     duplicate_str = re.search( r'duplicate\s+of\s+#([0-9]+)', comment_str)
-                    duplicate_2_str = re.search( r'dupli', comment_str)
+                    if not duplicate_str :
+                        duplicate_str = re.search( r'duplicate\s+of\s+'+PE_issues_regex, comment_str)
                     if duplicate_str:
                         parent_issue_number = int(duplicate_str.group(1))
                         break
@@ -489,7 +596,7 @@ class PeProcessing(BaseController):
                         audit[issue.number].append(0)
 
             else:
-                audit[issue.number].extend([0,0,0])
+                audit[issue.number].extend([0,0,0,0])
 
         # Printing into csv
 
@@ -502,8 +609,8 @@ class PeProcessing(BaseController):
         wr.writerow(CSV_HEADER)
 
         for issue in all_issues:
-            assert(len(audit[issue.number])==(len(CSV_HEADER)-1))
-            to_print = [issue.number]+audit[issue.number]
+            assert(len(audit[issue.number])==(len(CSV_HEADER)-2))
+            to_print = [issue.number]+audit[issue.number]+[int(sum(audit[issue.number][:7])>0)]
             wr.writerow(to_print)
 
 
